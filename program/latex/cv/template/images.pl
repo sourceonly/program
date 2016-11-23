654d03829e997d8cb5459258471c331f53a7344e
Source <sourceonly@gmail.com>
2016-11-23 21:12:05 +0800

    1. addjust resume

---
+++ program/latex/cv/template/images.pl
@@ -0,0 +1,47 @@
+# LaTeX2HTML 2008 (1.71)
+# Associate images original text with physical files.
+
+
+$key = q/{twenty}<comment_mark>twentyitem{since1865}{Ph.D.{{normalfont{candidateinComputeool}{Wonderland}{Specializinginmathematicsandphysics.}<comment_mark>{twenty};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="555" HEIGHT="170" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img1.png"
+ ALT="\begin{twenty}%
+\twentyitem{since 1865}{Ph.D. {\normalfont candidate in Computer...
+...gh school}{Wonderland}{Specializing in mathematics and physics.}
+%
+\end{twenty}">|; 
+
+$key = q/{twenty}<comment_mark>twentyitem{1900}{AliceinWonderland-TheCircra(1900's)SilentversioniswithoutadoubtthemostfamousAliceadaptionmade.}<comment_mark>{twenty};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="553" HEIGHT="220" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img4.png"
+ ALT="\begin{twenty}%
+\twentyitem{1900}{Alice in Wonderland-The Circra (1900's) Silent...
+...version is without a doubt the most famous Alice adaption made.}
+%
+\end{twenty}">|; 
+
+$key = q/{twentyshort}<comment_mark>twentyitemshort{1987}{All-TimeBestFantasyNovel.}twentmshort{1998}{All-TimeBestFantasyNovelbefore1990.}<comment_mark>{twentyshort};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="386" HEIGHT="34" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img3.png"
+ ALT="\begin{twentyshort}%
+\twentyitemshort{1987}{All-Time Best Fantasy Novel.}
+\twentyitemshort{1998}{All-Time Best Fantasy Novel before 1990.}
+%
+\end{twentyshort}">|; 
+
+$key = q/{twentyshort}<comment_mark>twentyitemshort{1865}{ChapterOne,DowntheRabbitHole.}tmshort{1865}{ChapterFive,AdvicefromaCaterpillar.}<comment_mark>{twentyshort};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="445" HEIGHT="92" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img2.png"
+ ALT="\begin{twentyshort}%
+\twentyitemshort{1865}{Chapter One, Down the Rabbit Hole.}
+...
+...tyitemshort{1865}{Chapter Five, Advice from a Caterpillar.}
+%
+\end{twentyshort}">|; 
+
+1;
+
