cdbe80d0017f345eb731a1b3b690a62118d0758d
source <source@debian.debian>
2017-09-03 14:52:23 -0600

    1. add cput_vs_time plot

---
+++ program/python/accounting/draw-cput.py
@@ -0,0 +1,28 @@
+import numpy as np
+import psycopg2 as pg
+
+conn=pg.connect("dbname=pbs_account user=source")
+cur=conn.cursor();
+
+
+
+def get_value_all(sq_ex):
+    cur.execute(sq_ex);
+    return cur.fetchall();
+def get_value(sq_ex,col=0):
+    cur.execute(sq_ex);
+    return map(lambda x:x[col],cur.fetchall());
+
+
+
+cput_array=get_value_all("select sum(cput),start_time from pbs_breakdown_full group by start_time order by start_time")
+
+time_axis=map(lambda x:x[1],cput_array);
+cput=map(lambda x:x[0]/3600,cput_array);
+
+import matplotlib.pyplot as plt;
+fig1,ax1  = plt.subplots();
+ax1.plot(time_axis,cput);
+
+plt.show();
+
